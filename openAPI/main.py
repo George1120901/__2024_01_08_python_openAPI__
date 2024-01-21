028a6ee1347f6aff05b41b9b4968ee0a9d465c83
George1120901 <pythonpro0901@gmail.com>
2024-01-21 22:38:50 +0800

    openAPI_2024_01_21_2238

--- openAPI/main.py
+++ openAPI/main.py
@@ -3,23 +3,26 @@ from fastapi import FastAPI
 import redis
 import os
 from dotenv import load_dotenv
+
+celsius=27
 load_dotenv()
 redis_conn = redis.Redis.from_url(os.environ.get('REDIS_HOST_PASSWORD'))
+redis_conn.set('board:temp', celsius)
 
 app = FastAPI()
 
-
 @app.get("/")
 def read_root():
-    counter = redis_conn.incr('test:increment',1)
-    return {"Counter": counter}
+    return {"pico_w": "temperture"}
+
+
+@app.get("/temperature/{celsius}")
+def counter(c: int):
+    celsius = redis_conn.get('board:temp')
+    return {"temperature": celsius}
+
 
 @app.get("/counter/{c}")
-def counter(c:int):
-    counter = redis_conn.incr('test:increment',c)
+def counter(c: int):
+    counter = redis_conn.incr('test:increment', c)
     return {"Counter": counter}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q:str | None = None):
-    return {"item_id": item_id, "q": q}
