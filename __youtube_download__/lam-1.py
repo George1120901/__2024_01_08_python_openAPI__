a60b61095549cd3e9afd89302cd8ba5adb167c64
George1120901 <pythonpro0901@gmail.com>
2024-04-23 12:00:57 +0800

    lambda  2404231200

--- __youtube_download__/lam-1.py
+++ __youtube_download__/lam-1.py
@@ -1,5 +1,8 @@
 #匿名函數lambda
 
+
+print('++++++++++++++++++++++++++')
+#lam-1.py
 #傳統函數 
 def f(x, y, z):
 	return x + y + z 
@@ -10,3 +13,17 @@ print(f(20, 30, 50))
 f = lambda x, y, z: x + y + z 
 print("匿名函數處理") 
 print(f(20, 30, 50))
+
+print('++++++++++++++++++++++++++')
+#lam-2.py
+#匿名函數當作參數傳入 - 1
+print("匿名函數當作參數傳入-1")
+mz = (lambda a , b  , c  : a + b + c) 
+print(mz('Wolfgang', ' Amadeus',' Mozart')) 
+
+#lam-2.py
+
+#匿名函數當作參數傳入-2
+print("匿名函數當作參數傳入-2")
+mz = (lambda a = 'Wolfgangus', b = ' Theophilus', c = ' Mozart': a + b + c) 
+print(mz('Wolf', ' Amadeus')) 
