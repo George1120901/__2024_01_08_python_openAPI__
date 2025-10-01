906a98df9cbef71259e37e7bd12f4a0a7d75d637
George1120901 <pythonpro0901@gmail.com>
2025-10-01 14:21:19 +0800

    pythonBasics_impory_251001_1421

---
+++ 00_selfStudy/imp_test.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""
+imtest
+~~~~~~
+For testing import vs. from-import
+"""
+x = 1
+y = 2
+
+# print('x is: {}'.format(x))
+print(f'x is: {x}')
+
+def fun():
+#    print('y is: {}'.format(y))
+    print(f'y is: {y}')
+
+def boy():
+    fun()
