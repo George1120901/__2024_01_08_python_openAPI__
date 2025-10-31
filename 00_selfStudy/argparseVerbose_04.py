286a97198d5c2a60423073d70b8bd6aaedfb88e0
George1120901 <pythonpro0901@gmail.com>
2025-10-31 14:01:57 +0800

    argparse_251031_1401

--- 00_selfStudy/argparseVerbose_04.py
+++ 00_selfStudy/argparseVerbose_04.py
@@ -9,9 +9,9 @@ parser.add_argument("-v",
 args = parser.parse_args()
 print(f"args.verbose 的值為：{args.verbose}")
 
-if args.verbose == 0  and args.verbose >= 4:
+if args.verbose == 0  or args.verbose >= 4:
     print('請輸入囉唆程度-選擇:1~3')
-elif args.verbose == 1 : 
+elif args.verbose == 1 :
     print('進入囉唆度 level1')
 elif args.verbose == 2 : 
     print('進入囉唆度 level2')
